dc70ad5bc329a47c4c4039f21318afd58625f77a
Justin Budlow <justin.budlow@gmail.com>
2020-03-22 01:22:47 -0400

    adding logic for saving the content in the configure round form and using that info to make the API call to get the right questions. still need to figure out what to do when no selection are made -- for a given option

--- js/app.js
+++ js/app.js
@@ -1,16 +1,50 @@
 $(function () {
 
-  generateQuestion()
+  $("#start-game").click((event) => {
+    console.log('starting a new game')
+    event.preventDefault()
 
-  function generateQuestion () {
-    const url = 'https://opentdb.com/api.php?amount=10'
+    let numberOfQuestions = $('#trivia_amount').val()
+    console.log(numberOfQuestions)
 
-    axios.get(url)
-    .then((response)=>{
-      console.log(response.data.results)
-    })
-    .catch((error)=>{
-      console.log(error)
-    })
-}
+    let category = $('#trivia_category').val()
+    console.log(category)
+
+    let difficulty = $('#trivia_difficulty').val()
+    console.log(difficulty)
+
+    let type = $('#trivia_type').val()
+    console.log(type)
+
+    $('.form-api').html('')
+
+    generateQuestions(numberOfQuestions, category, difficulty, type)
+
+  })
+
+  function generateQuestions(a,b,c,d) {
+      const url = "https://opentdb.com/api.php"
+
+      $.ajax({
+        url: url,
+        type: "GET",
+        data:
+        { amount: a,
+          category: b,
+          difficulty: c,
+          type: d
+        }
+      })
+      .done((response) => {
+        // execute this function if request is successful
+        console.log(response.results)
+        $('#top-paragraph').text(response.results[0].question)
+
+      })
+      .fail(() => {
+        // execute this function if request fails
+        alert('error occurred')
+      })
+    }
+
 })
